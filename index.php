efd960ccff012b1fa108c26c3f13dae8cfe06f33
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-15 17:21:56 -0200

    Fetchall

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ try {
   $query = "select * from products";
   // $query = "insert into products (name,descri) values('JS','JS in SON')";
   $stmt = $conn->query($query);
-  $list = $stmt->fetchAll();
-  echo $list[0][2];
+  $list = $stmt->fetchAll(PDO::FETCH_OBJ);
+  echo $list[2]->descri;
   // print_r($list);
 
 } catch (\PDOException $e) {
