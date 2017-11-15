a82b6449f86e4eba38854c34de9b27ba8b9d74f2
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-15 17:07:33 -0200

    Query

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 try {
   $conn = new \PDO("mysql:host=localhost;dbname=test_00","root","123mudar");
 
-  // $query = "select * from products";
-  $query = "insert into products (name,descri) values('JS','JS in SON')";
-  $rest = $conn->exec($query);
-  print_r($rest);
+  $query = "select * from products";
+  // $query = "insert into products (name,descri) values('JS','JS in SON')";
+  $stmt = $conn->query($query);
+  $list = $stmt->fetchAll();
+  echo $list[0][2];
+  // print_r($list);
 
 } catch (\PDOException $e) {
   echo "Erro! Message: ".$e->getMessage()." Code: ".$e->getCode();
