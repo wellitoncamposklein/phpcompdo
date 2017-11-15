7ed9b5118617058abb2f9718e21e99926afd9f9c
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-15 16:47:23 -0200

    Tratando exeptions

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+
+try {
   $conn = new \PDO("mysql:host=localhost;dbname=test_00","root","123mudar");
+} catch (\PDOException $e) {
+  echo "Erro! Message: ".$e->getMessage()." Code: ".$e->getCode();
+}
