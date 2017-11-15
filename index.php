7f90c6da5f035d4d7a3efe2acfb0edc2d0736c75
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-15 17:38:35 -0200

    Query on the run

--- index.php
+++ index.php
@@ -5,11 +5,10 @@ try {
 
   $query = "select * from products";
   // $query = "insert into products (name,descri) values('JS','JS in SON')";
-  $stmt = $conn->query($query);
-  // $list = $stmt->fetchAll(PDO::FETCH_OBJ);
-  $list = $stmt->fetch(PDO::FETCH_OBJ);
-  echo $list->descri;
-  // print_r($list);
+  foreach ($conn->query($query) as $product) {
+    echo $product['descri']."<br/>";
+  }
+
 
 } catch (\PDOException $e) {
   echo "Erro! Message: ".$e->getMessage()." Code: ".$e->getCode();
