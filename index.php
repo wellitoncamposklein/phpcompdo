3950889216281b1c9cba21d062e7d36a67161d7b
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-15 17:02:30 -0200

    Conhecendo o Exec

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 try {
   $conn = new \PDO("mysql:host=localhost;dbname=test_00","root","123mudar");
+
+  // $query = "select * from products";
+  $query = "insert into products (name,descri) values('JS','JS in SON')";
+  $rest = $conn->exec($query);
+  print_r($rest);
+
 } catch (\PDOException $e) {
   echo "Erro! Message: ".$e->getMessage()." Code: ".$e->getCode();
 }
