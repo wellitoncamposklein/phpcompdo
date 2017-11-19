18d9aab892842ea627892720cb188d24d58e8457
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-18 23:35:06 -0200

    SQL Injection

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 try {
   $conn = new \PDO("mysql:host=localhost;dbname=test_00","root","123mudar");
 
-  $query = "select * from products";
+  $query = "select * from products where id={$_GET['id']}";
   // $query = "insert into products (name,descri) values('JS','JS in SON')";
   foreach ($conn->query($query) as $product) {
+    echo $product['name']."<br/>";
     echo $product['descri']."<br/>";
   }
 
